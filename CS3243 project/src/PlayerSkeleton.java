bdeb714bea8a921a1f7246d076fec7bbcbc40f8d
unyoungwax <unyoungwax@hotmail.com>
2016-03-14 20:27:45 +0800

    Implemented pickMove

--- CS3243 project/src/PlayerSkeleton.java
+++ CS3243 project/src/PlayerSkeleton.java
@@ -1,9 +1,76 @@
 import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
+
+import java.util.Arrays;
 
 public class PlayerSkeleton {
+	
+	private static int[][] pWidth = {
+			{2},
+			{1,4},
+			{2,3,2,3},
+			{2,3,2,3},
+			{2,3,2,3},
+			{3,2},
+			{3,2}
+	};
+	
+	private static int[][] pHeight = {
+			{2},
+			{4,1},
+			{3,2,3,2},
+			{3,2,3,2},
+			{3,2,3,2},
+			{2,3},
+			{2,3}
+	};
+	private static int[][][] pBottom = {
+		{{0,0}},
+		{{0},{0,0,0,0}},
+		{{0,0},{0,1,1},{2,0},{0,0,0}},
+		{{0,0},{0,0,0},{0,2},{1,1,0}},
+		{{0,1},{1,0,1},{1,0},{0,0,0}},
+		{{0,0,1},{1,0}},
+		{{1,0,0},{0,1}}
+	};
+	private static int[][][] pTop = {
+		{{2,2}},
+		{{4},{1,1,1,1}},
+		{{3,1},{2,2,2},{3,3},{1,1,2}},
+		{{1,3},{2,1,1},{3,3},{2,2,2}},
+		{{3,2},{2,2,2},{2,3},{1,2,1}},
+		{{1,2,2},{3,2}},
+		{{2,2,1},{2,3}}
+	};
+
+	private static final int VECTOR_SIZE = 21;
+
+	private double[] vector = new double[VECTOR_SIZE];
 
 	public static final Double Lambda = 0.6;
+	
+	public PlayerSkeleton() {	
+		vector[0] = -1;
+		vector[1] = -1.5;
+		vector[2] = -2.3;
+		vector[3] = -3.4;
+		vector[4] = -5;
+		vector[5] = -5;
+		vector[6] = -3.4;
+		vector[7] = -2.3;
+		vector[8] = -1.5;
+		vector[9] = -1;
+		vector[10] = -0.5;
+		vector[11] = -1.2;
+		vector[12] = -2;
+		vector[13] = -4;
+		vector[14] = -4;
+		vector[15] = -2.4;
+		vector[16] = -2;
+		vector[17] = -1.2;
+		vector[18] = -0.5;
+		vector[19] = -5;
+		vector[20] = -1;
+	}
 	
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
@@ -28,36 +95,165 @@ public class PlayerSkeleton {
 		//					  eventually we can implement the weight vector learning function as described
 		//					  in page 15 of ref2.pdf
 		
-		return 0;
+		int optimalMove = 0;
+		double maxEvaluation = -Double.MAX_VALUE;
+
+		for (int i = 0; i < legalMoves.length; i++) {
+
+			double evaluation = simulate(s, i, vector);
+
+			if (evaluation > maxEvaluation) {
+				maxEvaluation = evaluation;
+				optimalMove = i;
+			}
+		}
+
+		return optimalMove;
+	}
+	
+	private double simulate(State state, int move, double[] vector) {
+
+		int[][] simulatedField = copyField(state.getField());
+		
+		int[] simulatedTop = Arrays.copyOf(state.getTop(), state.getTop().length);
+
+		double reward = makeMove(move, simulatedField, simulatedTop, state.getNextPiece());
+
+		double utility = calculateUtility(simulatedField, simulatedTop, vector);
+
+		return reward + utility;
+	}
+	
+	private double calculateUtility(int[][] field, int[] top, double[] vector) {
+		
+		int utility = 0;
+	
+		int[] features = new int[VECTOR_SIZE];
+	
+		for (int i = 0; i < State.COLS; i++) {
+			features[i] = getColHeight(top, i);
+		}
+	
+		for (int i = 0; i < State.COLS - 1; i++) {
+			features[State.COLS + i] = getAdjColHeight(top, i);
+		}
+	
+		features[19] = getMaxColHeight(top);
+		features[20] = getNumOfHoles(field, top);
+	
+		for (int i = 0; i < VECTOR_SIZE; i++) {
+			utility += vector[i] * features[i];
+		}
+	
+		return utility;
+	}
+	
+
+	// Modified from State.java
+	// Return the number of rows cleared, -1 if the game is lost
+	private int makeMove(int move, int[][] field, int[] top, int nextPiece) {
+		
+		int orient = State.legalMoves[nextPiece][move][State.ORIENT];
+		int slot = State.legalMoves[nextPiece][move][State.SLOT];
+		
+		//height if the first column makes contact
+		int height = top[slot]-pBottom[nextPiece][orient][0];
+		//for each column beyond the first in the piece
+		for(int c = 1; c < pWidth[nextPiece][orient];c++) {
+			height = Math.max(height,top[slot+c]-pBottom[nextPiece][orient][c]);
+		}
+		
+		//check if game ended
+		if(height+pHeight[nextPiece][orient] >= State.ROWS) {
+			return -1;
+		}
+		
+		//for each column in the piece - fill in the appropriate blocks
+		for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
+			
+			//from bottom to top of brick
+			for(int h = height+pBottom[nextPiece][orient][i]; h < height+pTop[nextPiece][orient][i]; h++) {
+				field[h][i+slot] = 1;
+			}
+		}
+		
+		//adjust top
+		for(int c = 0; c < pWidth[nextPiece][orient]; c++) {
+			top[slot+c]=height+pTop[nextPiece][orient][c];
+		}
+		
+		int rowsCleared = 0;
+		
+		//check for full rows - starting at the top
+		for(int r = height+pHeight[nextPiece][orient]-1; r >= height; r--) {
+			//check all columns in the row
+			boolean full = true;
+			for(int c = 0; c < State.COLS; c++) {
+				if(field[r][c] == 0) {
+					full = false;
+					break;
+				}
+			}
+			//if the row was full - remove it and slide above stuff down
+			if(full) {
+				rowsCleared++;
+				//for each column
+				for(int c = 0; c < State.COLS; c++) {
+	
+					//slide down all bricks
+					for(int i = r; i < top[c]; i++) {
+						field[i][c] = field[i+1][c];
+					}
+					//lower the top
+					top[c]--;
+					while(top[c]>=1 && field[top[c]-1][c]==0)	top[c]--;
+				}
+			}
+		}
+		
+		return rowsCleared;
+	}
+
+	private int[][] copyField(int[][] originalField) {
+
+		int[][] simulatedField = new int[State.ROWS][State.COLS + 1];
+
+		for (int i = 0; i < State.ROWS; i++) {
+			for (int j = 0; j < State.COLS; j++) {
+				simulatedField[i][j] = originalField[i][j];
+			}
+		}
+
+		return simulatedField;
 	}
 	
 	/**
-	 * @param s
+	 * @param top
 	 * @param col
 	 * @return corresponding column height of the wall
 	 */
-	public int getColHeight(State s, int col) {
-	    return s.getTop()[col];
+	public int getColHeight(int[] top, int col) {
+	    return top[col];
 	}
 	
 	/**
-	 * @param s
+	 * @param top
 	 * @param col
 	 * @return absolute difference between adjacent column heights
 	 */
-	public int getAdjColHeight(State s, int col) {
-	    return s.getTop()[col] - s.getTop()[col + 1];
+	public int getAdjColHeight(int[] top, int col) {
+	    return Math.abs(top[col] - top[col + 1]);
 	}
 	
 	/**
-	 * @param s
+	 * @param top
 	 * @return maximum column height
 	 */
-	public int getMaxColHeight(State s) {
+	public int getMaxColHeight(int[] top) {
 	    int max = 0;
-	    for (int i = 0; i < s.COLS; i++) {
-	        if (getColHeight(s, i) > max) {
-	            max = getColHeight(s, i);
+	    for (int i = 0; i < State.COLS; i++) {
+	        if (getColHeight(top, i) > max) {
+	            max = getColHeight(top, i);
 	        }
 	    }
 	    
@@ -65,20 +261,21 @@ public class PlayerSkeleton {
 	}
 	
 	/**
-	 * @param s
+	 * @param field
+	 * @param top
 	 * @return number of holes
 	 */
-	public int getNumOfHoles(State s) {
+	public int getNumOfHoles(int[][] field, int[] top) {
 		int holes = 0;
 		
 		// to add a dummy boundary along the 10th column's right side.
-		for(int k = 0; k < s.getTop[9]; k++) {		// the top of the 10th column
-			s.getField()[k][10] = 1;				// to signify the right boundary of the 10th column
+		for(int k = 0; k < top[9]; k++) {		// the top of the 10th column
+			field[k][10] = 1;				// to signify the right boundary of the 10th column
 		}
 		
-		for(int i = 0; i < s.COLS; i++) {
-			for(int j = 0; j < s.getTop()[i]; j++) {
-				if(s.getField()[j][i] == 0 && s.getField()[j+1][i+1] != 0 && s.getField()[j][i+1] != 0) {
+		for(int i = 0; i < State.COLS; i++) {
+			for(int j = 0; j < top[i]; j++) {
+				if(field[j][i] == 0 && field[j+1][i+1] != 0 && field[j][i+1] != 0) {
 					holes++;
 				}
 			}
